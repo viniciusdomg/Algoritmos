349d6a2281241e129a2e11d4de5a4062b1ebc32b
viniciusdomg <130383526+viniciusdomg@users.noreply.github.com>
2023-05-29 17:21:30 -0300

    Create quest09.cpp

---
+++ lista04/quest09.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+/*
+Elabore um algoritmo que leia um vetor A de 10 elementos e construa um vetor P formado pelos 
+índices dos elementos pares de A. Exemplo: Se A = [1 3 6 7 8], seus elementos pares estão nos
+índices 2 e 4 . Assim, P = [2 4].
+*/
+
+int main(){
+	
+	int a[10], digito, i, mult, div, cont;
+	
+}

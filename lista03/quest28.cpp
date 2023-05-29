9c8a92a3bf23d94d065ee520dbad7535c5c6ee83
viniciusdomg <130383526+viniciusdomg@users.noreply.github.com>
2023-05-29 17:21:10 -0300

    Create quest28.cpp

---
+++ lista03/quest28.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*
+Elabore um algoritmo que calcule o número de dias decorridos entre duas datas lidas. Cada data é 
+formada por três valores: dia, mês e ano. Considerar meses de 28, 29, 30 e 31 dias e a ocorrência
+de anos bissextos (sabe-se que um ano é bissexto quando é divisível por 4 e não é divisível por 
+100, ou quando é divisível por 400). Se for digitada uma data inválida, o algoritmo deverá mostrar
+a mensagem “Erro, data inválida” e solicitar que o usuário digite novamente até que a data seja
+válida.
+*/
+
+int main(){
+	
+	int ano, mes, dia, diasDecorr;
+	
+	
+}

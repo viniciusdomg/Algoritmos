108987d2295e6cbf40e5529cd20b4a29a25f02b0
viniciusdomg <130383526+viniciusdomg@users.noreply.github.com>
2023-06-21 12:38:18 -0300

    Create quest.cpp

---
+++ lista06/quest.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+Uma empresa tem para cada um dos seus 200 funcionários uma ficha contendo o nome,
+número de horas trabalhadas e o número de dependentes. Considerando que:
+- A empresa paga 12 reais por hora e 40 reais por dependentes;
+- Sobre o salário são feitos descontos de 8,5% para o INSS e 5% para IR.
+Elabore um algoritmo que declare o registro do funcionário, e leia seus dados enquanto o
+usuário desejar cadastrar (não permitindo o cadastro de mais de 200 funcionários). Após a
+leitura, escreva o nome, salário bruto, os valores descontados para cada tipo de imposto e
+qual o salário líquido de cada um dos funcionários que foram cadastrados.
+*/
+
+int main(){
+	
+	
+}

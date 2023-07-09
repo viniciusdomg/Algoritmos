f2b5cb283292e078d77abde3ccf492ba264330df
viniciusdomg <130383526+viniciusdomg@users.noreply.github.com>
2023-07-08 23:50:01 -0300

    Update quest06.cpp

--- lista06/quest06.cpp
+++ lista06/quest06.cpp
@@ -3,95 +3,51 @@
 using namespace std;
 
 /*
-Elabore um algoritmo que defina um registro capaz de armazenar as seguintes informações
-sobre um determinado cliente de um banco: nome, CPF, RG, número da conta, data de
-abertura da conta e saldo. Em seguida manipule um vetor com 15 registros de clientes, onde
-cada registro é um elemento do tipo de dado definido. A manipulação do vetor é feita através
-das seguintes opções: cadastrar cliente, imprimir os dados de um determinado cliente com
-base no valor do campo CPF e imprimir todos os clientes com saldo negativo.
+Uma empresa tem para cada um dos seus 200 funcionários uma ficha contendo o nome,
+número de horas trabalhadas e o número de dependentes. Considerando que:
+- A empresa paga 12 reais por hora e 40 reais por dependentes;
+- Sobre o salário são feitos descontos de 8,5% para o INSS e 5% para IR.
+Elabore um algoritmo que declare o registro do funcionário, e leia seus dados enquanto o
+usuário desejar cadastrar (não permitindo o cadastro de mais de 200 funcionários). Após a
+leitura, escreva o nome, salário bruto, os valores descontados para cada tipo de imposto e
+qual o salário líquido de cada um dos funcionários que foram cadastrados.
 */
 
-struct banco{
+struct tEmpresa{
 	string nome;
-	string cpf;
-	int rg;
-	int numeroConta;
-	string dataAberturaC;
-	float saldo;
+	int horas;
+	int dependentes;
+	float salarioLiq;
 };
-
-const int tam=15;
 int main(){
+
+	tEmpresa funcionario[200];
+	int i, cont=0, opcao;
 	
-	banco c[tam];
-	int i=0, j, op, opcao;
-	string busca;	
-	
-	do{	
-		cout << "\e[H\e[2J";
-		system("cls");
-		cout << "----BANCO----";
-		cout << "\n\n1- Cadastrar Cliente";
-		cout << "\n2- Buscar Cliente";
-		cout << "\n3- Buscar Saldos Negativos";
-		cout << "\n4- Sair";
-		cout << "\n\nDigite a opcao que deseja: ";
-		cin >> op;
+	cout << "----CADASTRO----";
+	do{
+		cout << "\n\nDigite o nome do funcionario: ";
+		getline(cin, funcionario[cont].nome);
+		cout << "\nQuantidade de horas trabalhadas no mes: ";
+		cin >> funcionario[cont].horas;
+		cout << "\nQuantidade de dependentes: ";
+		cin >> funcionario[cont].dependentes;
+		funcionario[cont].salarioLiq = (funcionario[cont].horas*12)+(funcionario[cont].dependentes*40);
+		
+		cont++;
+		cout << "\n\nDeseja cadastrar outro funcionario? (1-sim, 2-nao)\n>> ";
+		cin >> opcao;
 		getchar();
-		switch(op){
-			case 1:	
-				do{
-					cout << "\n\nDigite o nome do cliente: ";
-					getline(cin, c[i].nome);
-					cout << "\nDigite o CPF do cliente: ";
-					getline(cin, c[i].cpf);
-					cout << "\nDigite o RG do cliente: ";
-					cin >> c[i].rg;
-					cout << "\nNumero da conta do cliente: ";
-					cin >> c[i].numeroConta;
-					cout << "\nDigite a data de abertura da conta do cliente: ";
-					getchar();
-					getline(cin, c[i].dataAberturaC);
-					cout << "\nSaldo do cliente: ";
-					cin >> c[i].saldo;
-					
-					cout << "\n\nDeseja cadastrar mais algum cliente? (1-sim, 0-nao) ";
-					cin >> opcao;
-					getchar();
-					if(opcao==1){
-						i++;
-					}
-				}while(opcao!=0 && i<tam);
-			break;
-			case 2: 
-				cout << "\n\nDigite o CPF do cliente que deseja: ";
-				getline(cin, busca);
-				for(j=0; j<=i; j++){
-					if(c[j].cpf==busca){
-						cout << "\n\nDigite o nome do cliente: " << c[j].nome;
-						cout << "\nDigite o CPF do cliente: " << c[j].cpf;
-						cout << "\nDigite o RG do cliente: " << c[j].rg;
-						cout << "\nNumero da conta do cliente: " << c[j].numeroConta;
-						cout << "\nDigite a data de abertura da conta do cliente: " << c[j].dataAberturaC;
-						cout << "\nSaldo do cliente: " << c[j].saldo;
-					}
-				}
-			break;
-			case 3: 
-				for(j=0; j<=i; j++){
-					if(c[j].saldo<0){
-						cout << "\n\nDigite o nome do cliente: " << c[j].nome;
-						cout << "\nDigite o CPF do cliente: " << c[j].cpf;
-						cout << "\nDigite o RG do cliente: " << c[j].rg;
-						cout << "\nNumero da conta do cliente: " << c[j].numeroConta;
-						cout << "\nDigite a data de abertura da conta do cliente: " << c[j].dataAberturaC;
-						cout << "\nSaldo do cliente: " << c[j].saldo;
-					}
-				}
-			case 4: cout << "\n\nFIM DO PROGRAMA\n";  
-			default: cout << "\n\nDIGITE UMA OPCAO VALIDA!!";
-		}
-	}while(op!=4);
+	}while(opcao!=2 && cont<200);
 	
+	system("cls");
+	cout << "----FUNCIONARIOS----";
+	for(i=0;i<cont; i++){
+		cout << "\n\nNome: " << funcionario[i].nome;
+		cout << "\nSalario bruto = " << funcionario[i].salarioLiq-((funcionario[i].salarioLiq*0.085)+(funcionario[i].salarioLiq*0.05));
+		cout << "\nSalario liquido = " << funcionario[i].salarioLiq;
+	}
+	
+	cout << "\n\nFIM DO PROGRAMA\n";
 	system("pause");
 }
